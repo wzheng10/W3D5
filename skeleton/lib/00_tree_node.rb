805ca08044c625a363f3905604b8431e1be4f050
wzheng10 <wzheng100@gmail.com>
2021-03-12 10:58:37 -0800

    w3d5

---
+++ skeleton/lib/00_tree_node.rb
@@ -0,0 +1,16 @@
+class PolyTreeNode
+
+    attr_reader :value, :parent, :children
+
+    def initialize(val)
+      @val = val
+      @parent = nil
+      @children = []
+    end
+
+    def parent=()
+
+    end
+
+end
+
