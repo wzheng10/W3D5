0e423b0f49a5301562bbfddd621c08ee22419d61
Daniel Cho <thedanielcho@gmail.com>
2021-03-12 14:32:14 -0500

    Work on parent= method

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -3,15 +3,30 @@ class PolyTreeNode
     attr_reader :value, :parent, :children
 
     def initialize(val)
-      @val = val
+      @value = val
       @parent = nil
       @children = []
     end
 
     def parent=(arg)
-
+      @parent = arg
+      if !arg.children.include?(self)
+        @parent.children << self
+      else
+        return nil
+      end
     end
-#testetsetestststetfdfdfdfdfsfsd
-#testing numbaaa two
+
 end
 
+
+#node1 {djalkthgjeajrtkldhf;jsalkf value="root" parent=nil children=[node2]} 
+
+#node2 {djalkthgjeajrtkldhf;jsalkf value="child1" parent=node1 children=[]}
+
+# self = node2
+
+# def parent=(node1)
+#   @parent = node1
+#   @parent.children << self 
+# end
