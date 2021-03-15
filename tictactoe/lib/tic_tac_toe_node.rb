a3d9894f276ddea0223c51cd82c9c7acfa809902
Daniel Cho <thedanielcho@gmail.com>
2021-03-15 17:20:35 -0400

    Finish children method

--- tictactoe/lib/tic_tac_toe_node.rb
+++ tictactoe/lib/tic_tac_toe_node.rb
@@ -3,14 +3,13 @@ require_relative 'tic_tac_toe'
 
 class TicTacToeNode
 
-  attr_reader :board, :next_mover_mark
+  attr_reader :board, :next_mover_mark, :prev_move_pos
 
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     # debugger
     @board = board
     @next_mover_mark = next_mover_mark
     @prev_move_pos = prev_move_pos
-    @children = []
     @parent = nil
   end
 
@@ -24,14 +23,24 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
-    debugger
-    board.each_with_index do |row, idx1|
-      row.each_with_index do |tile, idx2|
-        child_board = board.dup.map { |subArr| subArr.dup.map { |el| el.dup}}
-        self.children << TicTacToeNode.new(board,)
-      end
-
-
+    childs = []
+    if next_mover_mark == :x
+      move = :o
+    else
+      move = :x
     end
+    (0..2).each do |idx1|
+      (0..2).each do |idx2|
+        pos = [idx1, idx2]
+
+        child_board = board.dup
+        if child_board.empty?(pos)
+          child_board[pos] = next_mover_mark
+          childs << TicTacToeNode.new(child_board, move, pos)
+        end
+      end
+    
+    end
+    return childs
   end
 end
