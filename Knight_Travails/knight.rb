eaa5de2dffd8d1ac8a60232325a37ef250fd5848
Daniel Cho <thedanielcho@gmail.com>
2021-03-15 13:35:02 -0400

    Start build_move_tree method

--- Knight_Travails/knight.rb
+++ Knight_Travails/knight.rb
@@ -1,6 +1,7 @@
 require_relative "../skeleton/lib/00_tree_node.rb"
 class KnightPathFinder
 
+    attr_reader :root_node
     MOVES = [[1,2],[2,1],[-2,-1],[-1,-2],[1,-2],[2,-1],[-2,1],[-1,2]]
     def self.valid_moves(pos)
         possible_moves = []
@@ -18,16 +19,25 @@ class KnightPathFinder
     def initialize(start_pos)
         @root_node = PolyTreeNode.new(start_pos)
         @considered_positions = [start_pos]
-        build_move_tree
+        # build_move_tree
     end
 
     def build_move_tree
-
+        line = [@root_node]
+        while !line.empty?
+            testing = line.shift
+            children = new_move_positions(testing.value)
+            children.each do |child|
+                testing.add_child(PolyTreeNode.new(child))
+            end
+            line += testing.children
+        end
     end
 
     def new_move_positions(pos)
         possible_moves = KnightPathFinder.valid_moves(pos) #Can Class be interchanged with KnightPathFinder
         possible_moves.reject! { |move| @considered_positions.include?(move)}
         @considered_positions << possible_moves 
+        return possible_moves
     end
 end
